b9dc0ce6404ece3ea60532d02a267ae0e61a6e6c
Cinthia-Condori <Cinthia.condori@unas.edu.pe>
2025-03-17 18:01:20 -0500

    4 Corrigiendo permisos y mejorando el CI/CD

---
+++ rails_app/spec/example_epec.rb
@@ -0,0 +1,8 @@
+# filepath: /home/cinthia/proyecto_contrucion_fin/rails_app/spec/example_spec.rb
+require 'rails_helper'
+
+RSpec.describe 'Example Test' do
+  it 'verifica que 1 + 1 es igual a 2' do
+    expect(1 + 1).to eq(2)
+  end
+end
